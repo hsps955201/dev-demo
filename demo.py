d0b1d4302161c3d7cf7ec928511f4c81656480c3
hsps955201 <hsps955201@gmail.com>
2024-11-26 23:13:32 +0800

    #2: feat: add substract method

--- demo.py
+++ demo.py
@@ -2,4 +2,12 @@ def add(num1: int, num2: int) -> int:
     return num1 + num2
 
 
+def subtract(num1: int, num2: int) -> int:
+    return num1 - num2
+
+
+def multiply(num1: int, num2: int) -> int:
+    return num1 * num2
+
+
 print(add(1, 2))
